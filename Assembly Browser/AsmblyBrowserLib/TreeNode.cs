2553bc7a3d74b433671021c6b76c8d0cf8eca361
Marian <marianstowerst@gmail.com>
2021-11-07 18:53:48 +0300

    realized ICommand and OpenFile function

--- Assembly Browser/AsmblyBrowserLib/TreeNode.cs
+++ Assembly Browser/AsmblyBrowserLib/TreeNode.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AsmblyBrowserLib
 {
-    class TreeNode
+    public class TreeNode
     {
     }
 }
--- Assembly Browser/WpfAsmblyBrowser/MainPageVM.cs
+++ Assembly Browser/WpfAsmblyBrowser/MainPageVM.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using AsmblyBrowserLib;
+using Microsoft.Win32;
 
 namespace WpfAsmblyBrowser
 {
-    class MainPageVM
+    class MainPageVM : INotifyPropertyChanged
     {
+        public string OpenedFileName { get; set; } = "";
+        public IEnumerable<TreeNode> Data { get; set; }
+        public ICommand OpenFileCommand => new OpenFileCommand(StartAssembly);
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void StartAssembly()
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Assemblies|*.dll;*.exe",
+                Title = "Select assembly",
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog().Value)
+            {
+                OpenedFileName = openFileDialog.FileName;
+                OnPropertyChanged(OpenedFileName);
+            }
+        }
     }
 }
